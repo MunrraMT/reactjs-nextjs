eda4e0dd5ed6b07e49554c1916201d691a1c048a
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-08-03 17:59:59 -0400

    Aula 12

--- secao-02/react-app-01/src/App.js
+++ secao-02/react-app-01/src/App.js
@@ -3,21 +3,35 @@ import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
+  state = {
+    name: 'André Rodrigues M. dos Santos'
+  };
+
+  handleClick = () => {
+    this.setState({ name: 'André Rodrigues' });
+  };
+
+  handlePClick = (e) => {
+    e.preventDefault();
+    this.setState({ name: 'Munrrá' });
+  };
+
   render() {
+    const { name } = this.state;
+
     return (
       <div className='App'>
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p onClick={this.handleClick}>{name}</p>
           <a
+            onClick={this.handlePClick}
             className='App-link'
             href='https://reactjs.org'
             target='_blank'
             rel='noopener noreferrer'
           >
-            Learn React
+            Este é o link
           </a>
         </header>
       </div>
