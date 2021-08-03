f73946d34bdebf81423be383ce371c73174d5096
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-08-03 19:04:25 -0400

    Aula 15

--- secao-02/react-app-01/src/App.js
+++ secao-02/react-app-01/src/App.js
@@ -3,6 +3,7 @@ import { Component } from 'react';
 
 class App extends Component {
   state = {
+    counter: 0,
     posts: [
       { id: 1, title: 'O título 1', body: 'O corpo 1' },
       { id: 2, title: 'O título 2', body: 'O corpo 2' },
@@ -10,11 +11,46 @@ class App extends Component {
     ]
   };
 
-  render() {
+  setTimeoutUpdateTitle = null;
+  setTimeoutUpdateCount = null;
+
+  handleTimeoutTitle = () => {
     const { posts } = this.state;
+
+    posts[0].title = 'Novo título';
+
+    this.timeoutUpdate = setTimeout(() => {
+      this.setState({ posts });
+    }, 5000);
+  };
+
+  handleTimeoutCounter = () => {
+    const { counter } = this.state;
+
+    this.setTimeoutUpdateCount = setTimeout(() => {
+      this.setState({ counter: counter + 1 });
+    }, 1000);
+  };
+
+  componentDidMount() {
+    this.handleTimeoutTitle();
+  }
+
+  componentDidUpdate() {
+    this.handleTimeoutCounter();
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.setTimeoutUpdateCount);
+    clearTimeout(this.setTimeoutUpdateTitle);
+  }
+
+  render() {
+    const { posts, counter } = this.state;
 
     return (
       <div className='App'>
+        <h2>{counter}</h2>
         {posts.map((post) => (
           <section key={post.id} id={post.id}>
             <h1>{post.title}</h1>
