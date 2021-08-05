d57e6848d2369951d9a0adb7ffd6bbc1c59d6626
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-08-04 23:14:52 -0400

    Aula 29.5

--- secao-02/react-app-01/src/components/Button/index.js
+++ secao-02/react-app-01/src/components/Button/index.js
@@ -1,22 +1,14 @@
 import './styles.css';
 
-import { Component } from 'react';
-
-class Button extends Component {
-  render() {
-    const { children, handleClick, disabled } = this.props;
-
-    return (
-      <button
-        disabled={disabled}
-        className='btn-more-posts'
-        type='button'
-        onClick={handleClick}
-      >
-        {children}
-      </button>
-    );
-  }
-}
+const Button = ({ disabled, handleClick, children }) => (
+  <button
+    disabled={disabled}
+    className='btn-more-posts'
+    type='button'
+    onClick={handleClick}
+  >
+    {children}
+  </button>
+);
 
 export default Button;
