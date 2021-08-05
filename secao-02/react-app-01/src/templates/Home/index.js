e808e4bcc35bec0981e0aaa8a08920488ff76afd
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-08-04 23:12:24 -0400

    Aula 29

--- secao-02/react-app-01/src/templates/Home/index.js
+++ secao-02/react-app-01/src/templates/Home/index.js
@@ -1,104 +1,180 @@
 import './styles.css';
 
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 
 import loadPosts from '../../utils/load-posts';
 import Posts from '../../components/Posts';
 import Button from '../../components/Button';
 import TextInput from '../../components/TextInput';
 
-class Home extends Component {
-  state = {
-    posts: [],
-    allPosts: [],
-    page: 0,
-    postsPerPage: 3,
-    searchValue: ''
-  };
+const Home = () => {
+  const [posts, setPosts] = useState([]);
+  const [allPosts, setaAllPosts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [postsPerPage] = useState(3);
+  const [searchValue, setSearchValue] = useState('');
 
-  loadPosts = async () => {
-    const { page, postsPerPage } = this.state;
+  useEffect(() => {
+    const handleLoadPosts = async () => {
+      const postsAndPhotos = await loadPosts();
 
-    const postsAndPhotos = await loadPosts();
+      setPosts(postsAndPhotos.slice(page, postsPerPage));
+      setaAllPosts(postsAndPhotos);
+    };
 
-    this.setState({
-      posts: postsAndPhotos.slice(page, postsPerPage),
-      allPosts: postsAndPhotos
-    });
-  };
+    handleLoadPosts();
+  }, []);
 
-  loadMorePosts = () => {
-    const { page, postsPerPage, allPosts, posts } = this.state;
-
+  const loadMorePosts = () => {
     const nextPage = page + postsPerPage;
     const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
 
-    posts.push(...nextPosts);
-
-    this.setState({ posts, page: nextPage });
+    setPosts([...posts, ...nextPosts]);
+    setPage(nextPage);
   };
 
-  handleInput = (e) => {
+  const handleInput = (e) => {
     const { value } = e.target;
-    this.setState({ searchValue: value });
+    setSearchValue(value);
   };
 
-  async componentDidMount() {
-    await this.loadPosts();
-  }
+  const noMorePosts = posts.length === allPosts.length;
 
-  componentDidUpdate() {
-    console.log(this.props['test-props']);
-  }
-
-  render() {
-    const { posts, allPosts, searchValue } = this.state;
-
-    const noMorePosts = posts.length === allPosts.length;
-
-    const filteredPosts = allPosts.filter((post) => {
-      return (
-        post.title.toLowerCase().indexOf(searchValue.toLowerCase()) !== -1 ||
-        post.body.toLowerCase().indexOf(searchValue.toLowerCase()) !== -1
-      );
-    });
-
+  const filteredPosts = allPosts.filter((post) => {
     return (
-      <>
-        <header
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-            padding: '1rem',
-            height: '7rem'
-          }}
-        >
-          <h1>Estudo de ReactJS - App Posts</h1>
-          <TextInput searchValue={searchValue} handleInput={this.handleInput} />
-        </header>
-
-        <main className='container'>
-          {searchValue ? (
-            (filteredPosts.length === 0 && <p>Busca não encontrada</p>) ||
-            (filteredPosts.length > 0 && <Posts posts={filteredPosts} />)
-          ) : (
-            <Posts posts={posts} />
-          )}
-        </main>
-
-        {!searchValue && (
-          <footer>
-            <Button disabled={noMorePosts} handleClick={this.loadMorePosts}>
-              Ver mais posts
-            </Button>
-          </footer>
-        )}
-      </>
+      post.title.toLowerCase().indexOf(searchValue.toLowerCase()) !== -1 ||
+      post.body.toLowerCase().indexOf(searchValue.toLowerCase()) !== -1
     );
-  }
-}
+  });
+
+  return (
+    <>
+      <header
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          padding: '1rem',
+          height: '7rem'
+        }}
+      >
+        <h1>Estudo de ReactJS - App Posts</h1>
+        <TextInput searchValue={searchValue} handleInput={handleInput} />
+      </header>
+
+      <main className='container'>
+        {searchValue ? (
+          (filteredPosts.length === 0 && <p>Busca não encontrada</p>) ||
+          (filteredPosts.length > 0 && <Posts posts={filteredPosts} />)
+        ) : (
+          <Posts posts={posts} />
+        )}
+      </main>
+
+      {!searchValue && (
+        <footer>
+          <Button disabled={noMorePosts} handleClick={loadMorePosts}>
+            Ver mais posts
+          </Button>
+        </footer>
+      )}
+    </>
+  );
+};
+
+// class Home2 extends Component {
+//   state = {
+//     posts: [],
+//     allPosts: [],
+//     page: 0,
+//     postsPerPage: 3,
+//     searchValue: ''
+//   };
+
+//   loadPosts = async () => {
+//     const { page, postsPerPage } = this.state;
+
+//     const postsAndPhotos = await loadPosts();
+
+//     this.setState({
+//       posts: postsAndPhotos.slice(page, postsPerPage),
+//       allPosts: postsAndPhotos
+//     });
+//   };
+
+//   loadMorePosts = () => {
+//     const { page, postsPerPage, allPosts, posts } = this.state;
+
+//     const nextPage = page + postsPerPage;
+//     const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
+
+//     posts.push(...nextPosts);
+
+//     this.setState({ posts, page: nextPage });
+//   };
+
+//   handleInput = (e) => {
+//     const { value } = e.target;
+//     this.setState({ searchValue: value });
+//   };
+
+//   async componentDidMount() {
+//     await this.loadPosts();
+//   }
+
+//   componentDidUpdate() {
+//     console.log(this.props['test-props']);
+//   }
+
+//   render() {
+//     const { posts, allPosts, searchValue } = this.state;
+
+//     const noMorePosts = posts.length === allPosts.length;
+
+//     const filteredPosts = allPosts.filter((post) => {
+//       return (
+//         post.title.toLowerCase().indexOf(searchValue.toLowerCase()) !== -1 ||
+//         post.body.toLowerCase().indexOf(searchValue.toLowerCase()) !== -1
+//       );
+//     });
+
+//     return (
+//       <>
+//         <header
+//           style={{
+//             display: 'flex',
+//             flexDirection: 'column',
+//             alignItems: 'center',
+//             justifyContent: 'center',
+//             padding: '1rem',
+//             height: '7rem'
+//           }}
+//         >
+//           <h1>Estudo de ReactJS - App Posts</h1>
+//           <TextInput searchValue={searchValue} handleInput={this.handleInput} />
+//         </header>
+
+//         <main className='container'>
+//           {searchValue ? (
+//             (filteredPosts.length === 0 && <p>Busca não encontrada</p>) ||
+//             (filteredPosts.length > 0 && <Posts posts={filteredPosts} />)
+//           ) : (
+//             <Posts posts={posts} />
+//           )}
+//         </main>
+
+//         {!searchValue && (
+//           <footer>
+//             <Button disabled={noMorePosts} handleClick={this.loadMorePosts}>
+//               Ver mais posts
+//             </Button>
+//           </footer>
+//         )}
+//       </>
+//     );
+//   }
+// }
 
 // function App() {
 //   return (
