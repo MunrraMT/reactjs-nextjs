25dddc7ef6fcf8065cb10d81ba4fc0a30c36808b
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-08-10 11:54:18 -0400

    Aula 42

--- secao-04/react-app-02/src/App.js
+++ secao-04/react-app-02/src/App.js
@@ -1,22 +1,40 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [reverse, setReverse] = useState(false);
+  const [counter, setCounter] = useState(0);
+
+  const reverseClass = reverse ? 'App-logo reverse' : 'App-logo';
+
+  const reverseToggle = () => setReverse(!reverse);
+  const handleIncrement = () => setCounter((prev) => prev + 1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className={reverseClass} alt='logo' />
+
+        <h1>Contador: {counter}</h1>
+
+        <button
+          onClick={reverseToggle}
+          style={{ fontSize: '2.5rem' }}
+          type='button'
         >
-          Learn React
-        </a>
+          {reverseClass}
+        </button>
+
+        <hr />
+
+        <button
+          onClick={handleIncrement}
+          style={{ fontSize: '2.5rem' }}
+          type='button'
+        >
+          Increment {counter}
+        </button>
       </header>
     </div>
   );
