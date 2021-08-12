60eb6d5913eb6126ce697307ce2e46e65e79d33d
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-08-12 08:28:46 -0400

    Aula 50

--- secao-04/react-app-02/src/Hooks/UseContext.js
+++ secao-04/react-app-02/src/Hooks/UseContext.js
@@ -3,68 +3,16 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable react/prop-types */
 
-import { createContext, useContext, useState } from 'react';
+import { GlobalContextProvider } from '../components/useContext/providers/GlobalContext';
+import Div from '../components/useContext/Div';
 import '../App.css';
 
-const globalState = {
-  title: 'Título do contexto',
-  body: 'Body do contexto',
-  counter: 0,
-};
-const GlobalContext = createContext();
-
-const H1 = () => {
-  const {
-    contextState: { title },
-  } = useContext(GlobalContext);
-
-  return <h1 className='App'>{title}</h1>;
-};
-
-const H2 = () => {
-  const {
-    contextState: { counter },
-  } = useContext(GlobalContext);
-
-  return <h2 className='App'>{counter}</h2>;
-};
-
-const P = () => {
-  const {
-    contextState: { body, counter },
-    contextState,
-    setContextState,
-  } = useContext(GlobalContext);
-
-  return (
-    <p
-      onClick={() => {
-        setContextState((prev) => ({ ...prev, counter: prev.counter + 1 }));
-      }}
-    >
-      {body}
-    </p>
-  );
-};
-
-const Div = () => (
-  <>
-    <H1 />
-    <P />
-    <H2 />
-  </>
+const UseContext = () => (
+  <main className='App'>
+    <GlobalContextProvider>
+      <Div />
+    </GlobalContextProvider>
+  </main>
 );
-
-const UseContext = () => {
-  const [contextState, setContextState] = useState(globalState);
-
-  return (
-    <main className='App'>
-      <GlobalContext.Provider value={{ contextState, setContextState }}>
-        <Div />
-      </GlobalContext.Provider>
-    </main>
-  );
-};
 
 export default UseContext;
---
+++ secao-04/react-app-02/src/components/useContext/Div/index.js
@@ -0,0 +1,13 @@
+import H1 from '../H1';
+import H2 from '../H2';
+import P from '../P';
+
+const Div = () => (
+  <>
+    <H1 />
+    <P />
+    <H2 />
+  </>
+);
+
+export default Div;
