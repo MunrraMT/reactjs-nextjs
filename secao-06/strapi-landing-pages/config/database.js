58985f52b3d07b6fdb369aea157cfeb7d0576097
André Rodrigues Melgaço dos Santos <dev.armsantos@gmail.com>
2022-09-15 09:54:26 -0400

    Reset module 6

--- secao-06/strapi-landing-pages/config/database.js
+++ secao-06/strapi-landing-pages/config/database.js
@@ -1,13 +1,17 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: "postgres",
     connection: {
-      host: env('DATABASE_HOST', 'rajje.db.elephantsql.com'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'ugngfrze'),
-      user: env('DATABASE_USERNAME', 'ugngfrze'),
-      password: env('DATABASE_PASSWORD', 'zYSbYEQJqijAV3afICgEsDVMLCqFho9V'),
-      ssl: env.bool('DATABASE_SSL', true),
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
+      ssl: env.bool("DATABASE_SSL"),
+    },
+    pool: {
+      min: 0,
+      max: 5,
     },
   },
 });
